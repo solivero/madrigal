b98a9495fe2665190b3d01b119038436535ccbff
Fredrik Sebek <fredriksebek@gmail.com>
2021-04-23 22:33:06 +0200

    -UX upgrades.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Ctx } from 'boardgame.io';
 import { Client } from 'boardgame.io/react';
 import { Madrigal } from './Game';
 import { GameState, Card, Board } from './Board'
+import React, { MouseEvent, useState } from 'react';
+import { range, reverse } from 'lodash'
 import React from 'react';
 import { range } from 'lodash'
 
@@ -16,6 +18,7 @@ interface Props {
 
 const cardStyle: React.CSSProperties = {
   border: '2px solid #555',
+  borderRadius: '5px',
   width: '90px',
   height: '135px',
   lineHeight: '50px',
@@ -25,21 +28,53 @@ const cardStyle: React.CSSProperties = {
 
 type Player = 'p0' | 'p1'
 
+const playerHandCardStyle: React.CSSProperties = {
+  border: '2px solid #555',
+  borderRadius: '5px',
+  width: '90px',
+  height: '135px',
+  lineHeight: '50px',
+  textAlign: 'center',
+  backgroundColor: 'lightgray',
+  margin: 20,
+}
+
+
+
+
 const CardRender = ({ card }: { card: Card }) => (
-  <div style={cardStyle}>{card.name}</div>
+  <button style={{ ...cardStyle, ...{ transform: 'rotate(20deg)' } }}>{card.name}</button>
 )
 
-function PlayerHand(props: { player: Player, hand: Card[] }): JSX.Element {
+const PlayerHandCardRender = ({ index, playerHandCardCount, card }: { index: number, playerHandCardCount: number, card: Card }) => {
+
+  return (
+    <button
+      className="Card-Hand"
+      onClick={() => {
+
+      }}
+    >
+      {card.name}</button>
+  )
+}
+
+function PlayerHand(props: { player: 'p0' | 'p1', hand: Card[] }): JSX.Element {
+  const playerHandCardCount = props.hand.length;
+
+  const [activeCardId, setActiveCardId] = useState(null);
+
   return <div style={{ margin: 'auto', display: 'flex', justifyContent: 'center' }}>
-    {props.hand.map(card => <CardRender card={card} />)}
+    {props.hand.map((card, index) => <PlayerHandCardRender index={index} playerHandCardCount={playerHandCardCount} card={card} />)}
   </div>
 }
 
 
 const cardSlotStyle: React.CSSProperties = {
   border: '2px solid #555',
-  width: '100px',
-  height: '150px',
+  borderRadius: '2px',
+  width: '75px',
+  height: '110px',
   lineHeight: '50px',
   textAlign: 'center',
 }
@@ -48,10 +83,11 @@ function CardSlot({ color, card }: { color: string, card?: Card }): JSX.Element 
   const style = {
     ...cardSlotStyle,
     borderColor: color,
+    backgroundColor: 'rgb(145,145,145, 0.5)'
   }
   return (
     <td style={style}>
-     {card && <CardRender card={card} />}
+      {card && <CardRender card={card} />}
     </td>
   )
 }
@@ -78,13 +114,18 @@ function MadrigalBoard({ G }: Props): JSX.Element {
   const p0 = G.players.p0
   const p1 = G.players.p1
   return (
-    <div>
+    <div style={{
+      position: 'fixed',
+      overflow: 'auto',
+      width: '100%',
+      height: '100%', backgroundImage: `url("background.jpg")`, backgroundSize: 'cover',
+    }}>
       <PlayerHand player="p0" hand={p0.hand} />
       <div style={{ height: 50 }}></div>
       <PlayerBoard player="p0" board={p0.board} />
-      <div style={{ height: 100 }}></div>
-      <PlayerBoard player="p1" board={p1.board} />
       <div style={{ height: 50 }}></div>
+      <PlayerBoard player="p1" board={p1.board} />
+      <div style={{ height: 10 }}></div>
       <PlayerHand player="p1" hand={p1.hand} />
     </div>
   );
