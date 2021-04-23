aac3cb1324771af963be865fae7138778fc98c42
Fredrik Sebek <fredriksebek@gmail.com>
2021-04-23 22:25:36 +0200

    Add card functions

---
+++ src/Cards.ts
@@ -0,0 +1,34 @@
+import { Card, CardColor } from "./Board"
+
+export const Priest = (color: CardColor): Card => ({
+    name: 'Spy',
+    basePoints: 1,
+    color,
+})
+
+export const Merchant = (color: CardColor): Card => ({
+    name: 'Merchant',
+    basePoints: 6,
+    color,
+})
+
+export const Fisherman = (color: CardColor): Card => ({
+    name: 'Fisherman',
+    basePoints: 3,
+    color,
+})
+export const Spy = (color: CardColor): Card => ({
+    name: 'Spy',
+    basePoints: 2,
+    color,
+})
+export const Thief = (color: CardColor): Card => ({
+    name: 'Thief',
+    basePoints: 1,
+    color,
+})
+export const Standard = (color: CardColor): Card => ({
+    name: 'Standard',
+    basePoints: 12,
+    color,
+})
