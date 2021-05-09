aa5c67d50fd83ba14889374a015c8e39c9605e53
Oliver Petri <oliver.ce.petri@gmail.com>
2021-05-09 17:41:31 +0200

    Eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
   rules: {
     // note you must disable the base rule as it can report incorrect errors
     'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': ['error']
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'space-before-function-paren': 'off'
   }
 }
