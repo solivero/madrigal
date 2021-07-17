e0c283d1876fe34ba9bdd6d1d6445c0cfbf2b403
Oliver Petri <oliver.ce.petri@gmail.com>
2021-07-16 21:07:05 -0700

    Rename image files to include background color

--- src/game/construct.ts
+++ src/game/construct.ts
@@ -1,192 +1,252 @@
-import fp from 'lodash/fp'
-import _ from 'lodash'
-import { Card, GameState, Board, PlayerState, Player, CardSlot, CardColor } from '../models'
-import { Spy1, Spy2, Fisherman, FieldMarshal, Farmer, Fog, Jester, Merchant, Warrior, Priest, Smith, Standard, Treasurer, Queen, King } from './cards'
+import fp from "lodash/fp";
+import _ from "lodash";
+import {
+  Card,
+  GameState,
+  Board,
+  PlayerState,
+  Player,
+  CardSlot,
+  CardColor,
+} from "../models";
+import {
+  Spy1,
+  Spy2,
+  Fisherman,
+  FieldMarshal,
+  Farmer,
+  Fog,
+  Jester,
+  Merchant,
+  Warrior,
+  Priest,
+  Smith,
+  Standard,
+  Treasurer,
+  Queen,
+  King,
+} from "./cards";
 
-import { Ctx } from 'boardgame.io'
+import { Ctx } from "boardgame.io";
 
-type GameStateProducer = (G: GameState) => GameState
+type GameStateProducer = (G: GameState) => GameState;
 
-function makeShuffledDeck (ctx: Ctx): Card[] {
-  const colors: CardColor[] = ['green', 'blue', 'red', 'gold']
-  const cardFuncs: ((color: CardColor) => Card)[] = [Spy1, Spy2, FieldMarshal, Fisherman, Farmer, Merchant, Warrior, Priest, Smith, Standard, Treasurer, Queen, King, Fog, Jester]
-  const cards = _.flatMap(cardFuncs, cardFunc => _.map(colors, cardFunc))
-  const deck = ctx.random?.Shuffle(cards)
+function makeShuffledDeck(ctx: Ctx): Card[] {
+  const colors: CardColor[] = ["green", "blue", "red", "gold"];
+  const cardFuncs: ((color: CardColor) => Card)[] = [
+    Spy1,
+    Spy2,
+    FieldMarshal,
+    Fisherman,
+    Farmer,
+    Merchant,
+    Warrior,
+    Priest,
+    Smith,
+    Standard,
+    Treasurer,
+    Queen,
+    King,
+    Fog,
+    Jester,
+  ];
+  const cards = _.flatMap(cardFuncs, (cardFunc) => _.map(colors, cardFunc));
+  const deck = ctx.random?.Shuffle(cards);
   if (deck) {
-    return deck
+    return deck;
   }
-  return []
+  return [];
 }
 
-function getPlayer (G: GameState, player: Player) {
-  return G.players[player]
+function getPlayer(G: GameState, player: Player) {
+  return G.players[player];
 }
 
-function getCardOnHand (G: GameState, player: Player, cardId: string) {
-  return G.players[player].hand.find(card => card.id === cardId)
+function getCardOnHand(G: GameState, player: Player, cardId: string) {
+  return G.players[player].hand.find((card) => card.id === cardId);
 }
 
-function updatePlayer (player: Player, updater: (playerState: PlayerState) => Partial<PlayerState>): GameStateProducer {
-  return fp.update(['players', player], (playerState: PlayerState) => ({
+function updatePlayer(
+  player: Player,
+  updater: (playerState: PlayerState) => Partial<PlayerState>
+): GameStateProducer {
+  return fp.update(["players", player], (playerState: PlayerState) => ({
     ...playerState,
-    ...updater(playerState)
-  }))
-}
-function updatePlayerBoard (player: Player, updater: (board: Board) => Partial<Board>): GameStateProducer {
-  return updatePlayer(player, ({ board }) => ({
-    board: {
-      ...board,
-      ...updater(_.cloneDeep(board))
-    }
-  }))
-}
-function getColumn (board: Board, colIdx: number): CardSlot[] {
-  return _.times(board.rows).map(rowIdx => board.cardSlots[(board.cols * rowIdx) + colIdx])
+    ...updater(playerState),
+  }));
 }
 
-function getRow (board: Board, rowIdx: number): CardSlot[] {
-  const startIdx = rowIdx * board.cols
-  const endIdx = startIdx + board.cols
-  return board.cardSlots.slice(startIdx, endIdx)
+function getColumn(board: Board, colIdx: number): CardSlot[] {
+  return _.times(board.rows).map(
+    (rowIdx) => board.cardSlots[board.cols * rowIdx + colIdx]
+  );
 }
 
-function addColumnBuffs (player: Player): GameStateProducer {
-  return updatePlayerBoard(player, board => {
-    const buffedBoard = board.cardSlots.map(slot => {
+function getRow(board: Board, rowIdx: number): CardSlot[] {
+  const startIdx = rowIdx * board.cols;
+  const endIdx = startIdx + board.cols;
+  return board.cardSlots.slice(startIdx, endIdx);
+}
+
+function addBuffs(player: Player): GameStateProducer {
+  return updatePlayer(player, ({ board }) => {
+    const buffedBoard = board.cardSlots.map((slot) => {
       if (!slot?.card) {
-        return slot
+        return slot;
       }
-      if (slot.card.isHero) {
-        return slot
-      }
-      const { card } = slot
-      const smithBuff = getSmithBuff(board, slot)
-      const columnBuff = getColumnBuff(board, slot)
-      const points = _.sum([card.basePoints, smithBuff, columnBuff])
+      const { card } = slot;
+      const buffs = [getColumnBuff, getSmithBuff, getFarmerBuff];
+      const appliedBuffs = buffs.map((buff) => buff(board, slot));
+      const buffPoints = _.sum(appliedBuffs);
       const buffedCard = {
         ...card,
-        points
-      }
+        points: slot.card.basePoints + buffPoints,
+      };
       return {
         ...slot,
-        card: buffedCard
-      }
-    })
-    return {
-      cardSlots: buffedBoard
-    }
-  })
-}
-
-function getColumnBuff (board: Board, slot: CardSlot): number {
-  const card = slot.card as Card
-  const colIdx = slot.index % board.cols
-  const col = getColumn(board, colIdx)
-  const sameCards = col.filter(cardSlot => cardSlot?.card && cardSlot.card.basePoints === card.basePoints)
-  const points = card.basePoints * (sameCards.length - 1)
-  return points
-}
-
-function getSmithBuff (board: Board, slot: CardSlot): number {
-  const rowIdx = Math.floor(slot.index / board.cols)
-  const row = getRow(board, rowIdx)
-  const smith = row.find(cardSlot => cardSlot?.card && cardSlot.card.basePoints === 5)
-  if (smith) {
-    return 1
-  }
-  return 0
-}
-
-function countPlayerPoints (player: Player): GameStateProducer {
-  const getSlotsWithCard = fp.filter((cardSlot: CardSlot) => Boolean(cardSlot?.card))
-  const getCards = fp.map((cardSlot: CardSlot) => cardSlot.card as Card)
-  const sumPoints = fp.sumBy((card: Card) => card.points)
-  const tallyBoardPoints = fp.flow(
-    getSlotsWithCard,
-    getCards,
-    sumPoints
-  )
-  return updatePlayer(player, (playerState: PlayerState) => ({
-    points: tallyBoardPoints(playerState.board.cardSlots)
-  }))
-}
-
-function addCardToBoard (player: Player, card: Card, boardCell: number) {
-  // Ridicoulus verbose to update deep structure while avoiding immer error
-  return updatePlayer(player, ({ board }) => {
-    const cardSlots = board.cardSlots.map(cardSlot => {
-      if (cardSlot.index === boardCell) {
-        return {
-          ...cardSlot,
-          card
-        }
-      }
-      return cardSlot
-    })
+        card: buffedCard,
+      };
+    });
     return {
       board: {
         ...board,
-        cardSlots
+        cardSlots: buffedBoard,
+      },
+    };
+  });
+}
+
+function getColumnBuff(board: Board, slot: CardSlot): number {
+  if (slot?.card?.isHero) {
+    return 0;
+  }
+  const card = slot.card as Card;
+  const colIdx = slot.index % board.cols;
+  const col = getColumn(board, colIdx);
+  const sameCards = col.filter(
+    (cardSlot) => cardSlot?.card && cardSlot.card.basePoints === card.basePoints
+  );
+  const points = card.basePoints * (sameCards.length - 1);
+  return points;
+}
+
+function getSmithBuff(board: Board, slot: CardSlot): number {
+  if (slot?.card?.basePoints === 7) {
+    return 0;
+  }
+  const rowIdx = Math.floor(slot.index / board.cols);
+  const row = getRow(board, rowIdx);
+  const smith = row.find(
+    (cardSlot) => cardSlot?.card && cardSlot.card.basePoints === 5
+  );
+  if (smith) {
+    return 1;
+  }
+  return 0;
+}
+
+function getFarmerBuff(board: Board, slot: CardSlot): number {
+  const farmer = board.cardSlots.find(
+    (cardSlot) => cardSlot?.card && cardSlot.card.basePoints === 4
+  );
+  if (farmer && slot?.card?.isHero) {
+    return 1;
+  }
+  return 0;
+}
+
+function countPlayerPoints(player: Player): GameStateProducer {
+  const getSlotsWithCard = fp.filter((cardSlot: CardSlot) =>
+    Boolean(cardSlot?.card)
+  );
+  const getCards = fp.map((cardSlot: CardSlot) => cardSlot.card as Card);
+  const sumPoints = fp.sumBy((card: Card) => card.points);
+  const tallyBoardPoints = fp.flow(getSlotsWithCard, getCards, sumPoints);
+  return updatePlayer(player, (playerState: PlayerState) => ({
+    points: tallyBoardPoints(playerState.board.cardSlots),
+  }));
+}
+
+function addCardToBoard(player: Player, card: Card, boardCell: number) {
+  // Ridicoulus verbose to update deep structure while avoiding immer error
+  return updatePlayer(player, ({ board }) => {
+    const cardSlots = board.cardSlots.map((cardSlot) => {
+      if (cardSlot.index === boardCell) {
+        return {
+          ...cardSlot,
+          card,
+        };
       }
-    }
-  })
+      return cardSlot;
+    });
+    return {
+      board: {
+        ...board,
+        cardSlots,
+      },
+    };
+  });
 }
 
-function addCardToHand (ctx: Ctx, player: Player, card: Card): GameStateProducer {
-  const id = `${player}-${ctx.random?.Number().toFixed(4)}`
-  const newCard = { ...card, id }
-  return updatePlayer(player, playerState => ({
-    hand: [...playerState.hand, newCard]
-  }))
+function addCardToHand(
+  ctx: Ctx,
+  player: Player,
+  card: Card
+): GameStateProducer {
+  const id = `${player}-${ctx.random?.Number().toFixed(4)}`;
+  const newCard = { ...card, id };
+  return updatePlayer(player, (playerState) => ({
+    hand: [...playerState.hand, newCard],
+  }));
 }
 
-function removeCardFromHand (player: Player, cardId: string): GameStateProducer {
-  return updatePlayer(player, playerState => ({
-    hand: playerState.hand.filter(card => card.id !== cardId)
-  }))
+function removeCardFromHand(player: Player, cardId: string): GameStateProducer {
+  return updatePlayer(player, (playerState) => ({
+    hand: playerState.hand.filter((card) => card.id !== cardId),
+  }));
 }
 
-function drawCard (ctx: Ctx, player: Player): GameStateProducer {
+function drawCard(ctx: Ctx, player: Player): GameStateProducer {
   return (G: GameState) => {
-    const card = _.head(G.deck)
+    const card = _.head(G.deck);
     if (card) {
-      const deck = _.tail(G.deck)
+      const deck = _.tail(G.deck);
       const gameState = {
         ...G,
-        deck
-      }
-      return addCardToHand(ctx, player, card)(gameState)
+        deck,
+      };
+      return addCardToHand(ctx, player, card)(gameState);
     }
-    return G
-  }
+    return G;
+  };
 }
 
-function setPlayerPassed (player: Player, passed: boolean): GameStateProducer {
+function setPlayerPassed(player: Player, passed: boolean): GameStateProducer {
   return updatePlayer(player, () => ({
-    passed
-  }))
+    passed,
+  }));
 }
 
-function incrementGame (player: Player): GameStateProducer {
-  return updatePlayer(player, playerState => ({
-    games: playerState.games + 1
-  }))
+function incrementGame(player: Player): GameStateProducer {
+  return updatePlayer(player, (playerState) => ({
+    games: playerState.games + 1,
+  }));
 }
 
-function boardToGraveyard (player: Player): GameStateProducer {
-  return updatePlayer(player, playerState => {
+function boardToGraveyard(player: Player): GameStateProducer {
+  return updatePlayer(player, (playerState) => {
     const boardCards = playerState.board.cardSlots
-      .filter(slot => slot.card)
-      .map(slot => slot.card as Card)
-    const emptyCardSlots = playerState.board.cardSlots.map(fp.omit('card'))
+      .filter((slot) => slot.card)
+      .map((slot) => slot.card as Card);
+    const emptyCardSlots = playerState.board.cardSlots.map(fp.omit("card"));
     return {
       graveyard: playerState.graveyard.concat(boardCards),
       board: {
         ...playerState.board,
-        cardSlots: emptyCardSlots
-      }
-    }
-  })
+        cardSlots: emptyCardSlots,
+      },
+    };
+  });
 }
 
 export {
@@ -201,5 +261,5 @@ export {
   setPlayerPassed,
   incrementGame,
   boardToGraveyard,
-  addColumnBuffs
-}
+  addBuffs,
+};
--- src/game/game.ts
+++ src/game/game.ts
@@ -1,88 +1,97 @@
-import { GameState, PlayerState, CardSlot, CellColor, Player } from '../models'
-import { Ctx, Game } from 'boardgame.io'
-import { countPlayerPoints, drawCard, getPlayer, makeShuffledDeck, boardToGraveyard, incrementGame, setPlayerPassed, addColumnBuffs } from './construct'
-import { playCard, pass } from './moves'
-import fp from 'lodash/fp'
-import _ from 'lodash'
+import { GameState, PlayerState, CardSlot, CellColor, Player } from "../models";
+import { Ctx, Game } from "boardgame.io";
+import {
+  countPlayerPoints,
+  drawCard,
+  getPlayer,
+  makeShuffledDeck,
+  boardToGraveyard,
+  incrementGame,
+  setPlayerPassed,
+  addBuffs,
+} from "./construct";
+import { playCard, pass } from "./moves";
+import fp from "lodash/fp";
+import _ from "lodash";
 
-function setup (ctx: Ctx): GameState {
-  const nCols = 7
-  function makeEmptyCardSlotRow (color: CellColor, row: number): CardSlot[] {
-    const rowStartIndex = row * nCols
+function setup(ctx: Ctx): GameState {
+  const nCols = 7;
+  function makeEmptyCardSlotRow(color: CellColor, row: number): CardSlot[] {
+    const rowStartIndex = row * nCols;
     return [
       {
         index: rowStartIndex,
-        color: 'neutral'
+        color: "neutral",
       },
-      ..._.range(1, 6).map(index => ({
+      ..._.range(1, 6).map((index) => ({
         index: rowStartIndex + index,
-        color
+        color,
       })),
       {
         index: rowStartIndex + 6,
-        color: 'neutral'
-      }
-    ]
+        color: "neutral",
+      },
+    ];
   }
 
-  const rowColors: CellColor[] = ['red', 'blue', 'green']
+  const rowColors: CellColor[] = ["red", "blue", "green"];
   const emptyPlayerState = (player: Player): PlayerState => {
-    const orderedRowColors = player === 'p0' ? rowColors : _.reverse(rowColors)
+    const orderedRowColors = player === "p0" ? rowColors : _.reverse(rowColors);
     return {
       board: {
-        cardSlots: _.flatMap<CellColor, CardSlot>(orderedRowColors, makeEmptyCardSlotRow),
+        cardSlots: _.flatMap<CellColor, CardSlot>(
+          orderedRowColors,
+          makeEmptyCardSlotRow
+        ),
         cols: nCols,
-        rows: rowColors.length
+        rows: rowColors.length,
       },
       hand: [],
       graveyard: [],
       points: 0,
       games: 0,
-      passed: false
-    }
-  }
+      passed: false,
+    };
+  };
   const gameState: GameState = {
     players: {
-      p0: emptyPlayerState('p0'),
-      p1: emptyPlayerState('p1')
+      p0: emptyPlayerState("p0"),
+      p1: emptyPlayerState("p1"),
     },
-    deck: makeShuffledDeck(ctx)
-  }
+    deck: makeShuffledDeck(ctx),
+  };
 
   const drawStartingCards = (player: Player) => {
-    return _.times(10).map(() => drawCard(ctx, player))
-  }
+    return _.times(10).map(() => drawCard(ctx, player));
+  };
   const addStartingCards = fp.flow([
-    ...drawStartingCards('p0'),
-    ...drawStartingCards('p1')
-  ])
+    ...drawStartingCards("p0"),
+    ...drawStartingCards("p1"),
+  ]);
 
-  return addStartingCards(gameState)
+  return addStartingCards(gameState);
 }
 
-function assignGameWin (G: GameState) {
-  const p0 = getPlayer(G, 'p0')
-  const p1 = getPlayer(G, 'p1')
+function assignGameWin(G: GameState) {
+  const p0 = getPlayer(G, "p0");
+  const p1 = getPlayer(G, "p1");
   // TODO handle tie. Last card played wins
-  const winningPlayer: Player = p0.points > p1.points ? 'p0' : 'p1'
+  const winningPlayer: Player = p0.points > p1.points ? "p0" : "p1";
   return fp.flow(
-    setPlayerPassed('p0', false),
-    setPlayerPassed('p1', false),
+    setPlayerPassed("p0", false),
+    setPlayerPassed("p1", false),
     incrementGame(winningPlayer)
-  )(G)
+  )(G);
 }
 
-function bothPassed (G: GameState) {
-  const p0 = getPlayer(G, 'p0')
-  const p1 = getPlayer(G, 'p1')
-  return p0.passed && p1.passed
+function bothPassed(G: GameState) {
+  const p0 = getPlayer(G, "p0");
+  const p1 = getPlayer(G, "p1");
+  return p0.passed && p1.passed;
 }
 
-function resetGame (G: GameState) {
-  return fp.flow(
-    boardToGraveyard('p0'),
-    boardToGraveyard('p1')
-  )(G)
+function resetGame(G: GameState) {
+  return fp.flow(boardToGraveyard("p0"), boardToGraveyard("p1"))(G);
 }
 
 const Madrigal: Game<GameState> = {
@@ -93,16 +102,16 @@ const Madrigal: Game<GameState> = {
       // const player = 'p' + ctx.currentPlayer as Player
       return fp.flow(
         // drawCard(ctx, player),
-        addColumnBuffs('p0'),
-        addColumnBuffs('p1'),
-        countPlayerPoints('p0'),
-        countPlayerPoints('p1')
-      )(G)
-    }
+        addBuffs("p0"),
+        addBuffs("p1"),
+        countPlayerPoints("p0"),
+        countPlayerPoints("p1")
+      )(G);
+    },
   },
   moves: {
     playCard,
-    pass
+    pass,
   },
   phases: {
     // TODO card exchange stage
@@ -110,30 +119,30 @@ const Madrigal: Game<GameState> = {
       start: true,
       endIf: bothPassed,
       onEnd: assignGameWin,
-      next: 'game2'
+      next: "game2",
     },
     game2: {
       endIf: bothPassed,
       onBegin: resetGame,
       onEnd: assignGameWin,
-      next: 'game3'
+      next: "game3",
     },
     game3: {
       endIf: bothPassed,
       onBegin: resetGame,
-      onEnd: assignGameWin
-    }
+      onEnd: assignGameWin,
+    },
   },
   endIf: (G, ctx) => {
     // TODO count gem. 2 wins
-    const playerWon = (player: Player) => getPlayer(G, player).games === 2
-    if (playerWon('p0')) {
-      return 'Player 1 won!'
+    const playerWon = (player: Player) => getPlayer(G, player).games === 2;
+    if (playerWon("p0")) {
+      return "Player 1 won!";
     }
-    if (playerWon('p1')) {
-      return 'Player 2 won!'
+    if (playerWon("p1")) {
+      return "Player 2 won!";
     }
-  }
-}
+  },
+};
 
-export { Madrigal }
+export { Madrigal };
